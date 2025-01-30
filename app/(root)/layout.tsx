d13ca13bcaf038ef5235db091fac1decf6138c18
Hariharan Jeganathan <hariharanj@lumel.com>
2025-01-30 18:39:55 +0530

    implemented user session

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+import Navbar from "../components/Navbar";
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return <main className="font-work-sans">
+    <Navbar/>
+    {children} 
+    </main>;
+};
+
+export default layout;
